15972265fbf28aa3eb35802bb63841961b40eebe
harisani <harisani02@gmail.com>
2022-08-29 18:48:02 +0700

    perbaikan button

--- lib/view/forgetPassword.dart
+++ lib/view/forgetPassword.dart
@@ -8,6 +8,24 @@ class ForgetPassoword extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        bottomSheet: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 10, 10, 50),
+          child: Container(
+            width: 500.0,
+              margin: EdgeInsets.fromLTRB(
+                  0, MediaQuery.of(context).size.height / 3, 0, 0),
+              height: 50,
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: Color.fromRGBO(175, 3, 4, 1)),
+                child: const Text(
+                  'KIRIM',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                onPressed: () {},
+              )),
+        ),
         appBar: AppBar(
           iconTheme: IconThemeData(color: Colors.black),
           title: Text(
@@ -17,8 +35,9 @@ class ForgetPassoword extends StatelessWidget {
           backgroundColor: Colors.white,
         ),
         body: Container(
-          padding: EdgeInsets.all(10),
-            margin: EdgeInsets.fromLTRB(0, MediaQuery.of(context).size.height / 4, 0, 0),
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.fromLTRB(
+                0, MediaQuery.of(context).size.height / 4, 0, 0),
             child: ListView(
               children: <Widget>[
                 Container(
@@ -28,19 +47,6 @@ class ForgetPassoword extends StatelessWidget {
                         InputDecoration(hintText: 'Masukan E-mail anda'),
                   ),
                 ),
-                Container(
-                margin: EdgeInsets.fromLTRB(0, MediaQuery.of(context).size.height / 3, 0, 0),
-                height: 50,
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: Color.fromRGBO(175, 3, 4, 1)),
-                  child: const Text(
-                    'KIRIM',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  onPressed: () {},
-                )),
               ],
             )));
   }
--- lib/view/signup.dart
+++ lib/view/signup.dart
@@ -8,95 +8,102 @@ class SignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomSheet: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 30, 10, 20),
+        child: Container(
+            width: 500.0,
+            // margin: EdgeInsets.fromLTRB(
+            //     0, MediaQuery.of(context).size.height / 3, 0, 0),
+            height: 50,
+            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  primary: Color.fromRGBO(175, 3, 4, 1)),
+              child: const Text(
+                'DAFTAR',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              onPressed: () {},
+            )),
+      ),
       appBar: AppBar(
         iconTheme: IconThemeData(color: Colors.black),
         title: Text(
           "Sign Up",
-          style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
         ),
         backgroundColor: Colors.white,
       ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-        child: ListView(
-          children: <Widget>[
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                    color: Color.fromRGBO(175, 3, 3, 1),
-                    shape: BoxShape.circle),
-                child:
-                    Image.asset("assets/images/icon_profile.png", width: 70.0)),
-            Container(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: 'Nama Lengkap',
-                  labelStyle: TextStyle(color: Colors.grey),
-                ),
+      body: ListView(
+        children: <Widget>[
+          Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 20)),
+          Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  color: Color.fromRGBO(175, 3, 3, 1), shape: BoxShape.circle),
+              child:
+                  Image.asset("assets/images/icon_profile.png", width: 70.0)),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Nama Lengkap',
+                labelStyle: TextStyle(color: Colors.grey),
               ),
             ),
-            Container(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: 'E-mail',
-                  labelStyle: TextStyle(color: Colors.grey),
-                ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'E-mail',
+                labelStyle: TextStyle(color: Colors.grey),
               ),
             ),
-            Container(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: 'Nomor Telepon',
-                  labelStyle: TextStyle(color: Colors.grey),
-                ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Nomor Telepon',
+                labelStyle: TextStyle(color: Colors.grey),
               ),
             ),
-            Container(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                    labelText: 'Password',
-                    labelStyle: TextStyle(color: Colors.grey)),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
                   labelText: 'Password',
-                  labelStyle: TextStyle(color: Colors.grey),
-                ),
+                  labelStyle: TextStyle(color: Colors.grey)),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                labelStyle: TextStyle(color: Colors.grey),
               ),
             ),
-            Container(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                decoration: InputDecoration(
-                    labelText: 'Gender',
-                    labelStyle: TextStyle(color: Colors.grey)),
-              ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              decoration: InputDecoration(
+                  labelText: 'Gender',
+                  labelStyle: TextStyle(color: Colors.grey)),
             ),
-            Container(
-                margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                height: 50,
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: Color.fromRGBO(175, 3, 4, 1)),
-                  child: const Text(
-                    'DAFTAR',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  onPressed: () {},
-                )),
-          ],
-        ),
+          ),
+          Container(
+            child: SizedBox(
+              height: 100,
+            ),
+          )
+        ],
       ),
     );
   }
